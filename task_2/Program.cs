ac0e06081cab2456e597a083053b4688c5dc179e
Pavel Kulik <Kanstantin0105@gmail.com>
2023-05-24 23:04:53 +0300

    task_2 add

---
+++ task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int SumNumber(int number)
+        {
+            int sum = 0;
+            while(number > 0)
+            {
+                sum += number % 10;
+                number = number / 10;
+            }
+            return sum;
+        }
+
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine()!);
+        Console.WriteLine($"{number} -> {SumNumber(number)}");
+    }
+}
