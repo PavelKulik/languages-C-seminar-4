e430407a4516f93357a2d5d3fee392e9bbc392b2
Pavel Kulik <Kanstantin0105@gmail.com>
2023-05-24 22:57:00 +0300

    task_1 add

---
+++ task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int Exponentiation(int number, int extent)
+        {
+            int exp = 1;
+            for (int i = 1; i <= extent; i++) { exp *= number; }
+            return exp;
+        }
+
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine()!);
+        Console.Write("Введите степень числа: ");
+        int extent = int.Parse(Console.ReadLine()!);
+
+        Console.WriteLine($"Число {number}^{extent} = {Exponentiation(number, extent)}");
+
+    }
+}
