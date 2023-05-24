4428fb9064ffb80e8123d707d45c99ff515e7047
Pavel Kulik <Kanstantin0105@gmail.com>
2023-05-24 23:12:13 +0300

    task_3 add

---
+++ task_3/Program.cs
@@ -0,0 +1,31 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] GetArray(int size)
+        {
+            int[] array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Введите элемен массива {i + 1}: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            return array;
+        }
+
+        void PrintArray(int[] array)
+        {
+            Console.Write("[");
+            foreach(int el in array)
+            {
+                Console.Write($"{el} ");
+            }
+            Console.WriteLine("]");
+        }
+
+        int[] array = GetArray(8);
+        PrintArray(array);
+    }
+}
